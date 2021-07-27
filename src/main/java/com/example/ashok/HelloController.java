21a1cdae1ba5ec1c5ba4681f9931386651b595a8
Ashok_Kumar <routashok04@gmail.com>
2021-07-27 20:35:16 +0530

    aws beanstalk first commit

---
+++ src/main/java/com/example/ashok/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.ashok;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    public String ShowAws(){
+        return "This is AWS Elastic beanStalk";
+    }
+}
